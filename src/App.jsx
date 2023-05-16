80c2f03746527852a58463d5817c4396afc69884
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-05-16 16:01:42 -0300

    Implementing cursor follow and toggle of body classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import { useState, useEffect } from 'react';
+import { Cursor } from './components/Cursor';
+import { Button } from './components/Button';
 import './App.css';
 
 function App() {
+  const [enabled, setEnabled] = useState(false);
 
-  return <></>;
+  // Toggle cursor on/off
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled);
+
+    return () => {
+      document.body.classList.remove('no-cursor');
+    };
+  }, [enabled]);
+
+  // TODO Toggle parallax
+
+  return (
+    <main>
+      <Cursor enabled={enabled} />
+      <Button enabled={enabled} setEnabled={setEnabled} />
+    </main>
+  );
 }
 
 export default App;
